c0542f656fc335b05988adbb0f32543580737a97
lagemaxl <85054751+lagemaxl@users.noreply.github.com>
2022-06-10 09:11:42 +0200

    Add files via upload

---
+++ kostky/kostky/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace kostky
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Playbill", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(445, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 123);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ROLL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::kostky.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(123, 123);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::kostky.Properties.Resources._1;
+            this.pictureBox2.Location = new System.Drawing.Point(158, 33);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(123, 123);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::kostky.Properties.Resources._1;
+            this.pictureBox3.Location = new System.Drawing.Point(287, 33);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(123, 123);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::kostky.Properties.Resources._1;
+            this.pictureBox4.Location = new System.Drawing.Point(29, 162);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(123, 123);
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::kostky.Properties.Resources._1;
+            this.pictureBox5.Location = new System.Drawing.Point(158, 162);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(123, 123);
+            this.pictureBox5.TabIndex = 2;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::kostky.Properties.Resources._1;
+            this.pictureBox6.Location = new System.Drawing.Point(287, 162);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(123, 123);
+            this.pictureBox6.TabIndex = 2;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Red;
+            this.pictureBox7.Location = new System.Drawing.Point(445, 33);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(153, 123);
+            this.pictureBox7.TabIndex = 3;
+            this.pictureBox7.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 307);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+    }
+}
+
---
+++ kostky/kostky/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kostky
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            Random random = new Random();
+            int[] pole = new int[6];
+            int[] cisla = new int[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                int a = random.Next(1, 6);
+                pole[i] = a;
+                cisla[i] = 0;
+
+            }
+
+            pictureBox1.Image = Image.FromFile("..\\..\\Resources\\" + pole[0] + ".jpg");
+            pictureBox2.Image = Image.FromFile("..\\..\\Resources\\" + pole[1] + ".jpg");
+            pictureBox3.Image = Image.FromFile("..\\..\\Resources\\" + pole[2] + ".jpg");
+            pictureBox4.Image = Image.FromFile("..\\..\\Resources\\" + pole[3] + ".jpg");
+            pictureBox5.Image = Image.FromFile("..\\..\\Resources\\" + pole[4] + ".jpg");
+            pictureBox6.Image = Image.FromFile("..\\..\\Resources\\" + pole[5] + ".jpg");
+
+
+            for (int i = 0; i < pole.Length; i++)
+            {
+                for (int j = 1; j < 6; j++)
+                {
+                    if(pole[i] == j)
+                    {
+                        cisla[j]++;
+                    }
+                }
+            }
+
+            string text = "";
+            for (int i = 0; i < pole.Length; i++)
+            {
+                if (cisla[i] != 0)
+                {
+                    text += cisla[i];
+                }
+            }
+
+            if(text.Contains("222"))
+            {
+                //MessageBox.Show("3 dvojce");
+                pictureBox7.BackColor = Color.Green;
+            }else if (text.Contains("3"))
+            {
+                //MessageBox.Show("trojce");
+                pictureBox7.BackColor = Color.Green;
+            } else if (text.Contains("111111"))
+            {
+                //MessageBox.Show("postupka");
+                pictureBox7.BackColor = Color.Green;
+            } else
+            {
+                pictureBox7.BackColor = Color.Red;
+            }
+
+
+
+
+            //MessageBox.Show(string.Join("", pole));
+            //MessageBox.Show(string.Join("", cisla));
+
+            
+
+        }
+    }
+}
